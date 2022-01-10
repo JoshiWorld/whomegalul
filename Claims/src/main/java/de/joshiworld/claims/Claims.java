c732fb91715e2b513467743b60c2ed4e05cc6d37
Joshua Stieber <joshua.stieber.1903@gmail.com>
2022-01-10 19:03:06 +0100

    Added Prefix

--- Claims/src/main/java/de/joshiworld/claims/Claims.java
+++ Claims/src/main/java/de/joshiworld/claims/Claims.java
@@ -3,15 +3,22 @@ package de.joshiworld.claims;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Claims extends JavaPlugin {
+    private static final String PREFIX = "§7[§eWHO§7]";
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+
+
+    // Prefix Getter
+    public static String getPrefix() {
+        return PREFIX;
     }
 }
