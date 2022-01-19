c53d4644ad87e37eb01cdfb84ce2cd2de1d8c111
Joshua Stieber <joshua.stieber.1903@gmail.com>
2022-01-19 20:35:21 +0100

    Basic Tablist

---
+++ TablistScore/src/main/java/de/joshiworld/main/TablistScore.java
@@ -0,0 +1,56 @@
+package de.joshiworld.main;
+
+import de.joshiworld.sql.MySQL;
+import net.luckperms.api.LuckPerms;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.RegisteredServiceProvider;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TablistScore extends JavaPlugin {
+    private final String PREFIX = "§7[§eTAB§7]";
+
+    private TablistScore plugin;
+    private LuckPerms luckperms;
+
+    private MySQL mysql;
+
+    @Override
+    public void onEnable() {
+        this.plugin = this;
+        connectSQL();
+        new InitStuff(this.plugin).init();
+
+        getLogger().info("§aTablist loaded");
+    }
+
+    @Override
+    public void onDisable() {
+        this.mysql.close();
+    }
+
+    public String getPrefix() {
+        return this.PREFIX;
+    }
+
+    public LuckPerms getLuckperms() {
+        return this.luckperms;
+    }
+
+    public MySQL getMySQL() {
+        return mysql;
+    }
+
+    // Init LuckPerms
+    public void initLuckPerms() {
+        RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+        if (provider != null) {
+            luckperms = provider.getProvider();
+        }
+    }
+
+    private void connectSQL() {
+        mysql = new MySQL("localhost", "who", "123whoMEGALUL?", "who");
+        mysql.update("CREATE TABLE IF NOT EXISTS who(PLAYER varchar(64), MONEY int, CLAIMS varchar(8000), TRUSTED varchar(1000), OTHERCLAIMS varchar(1000))");
+    }
+
+}
